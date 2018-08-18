afd0ce989855855e685a2b3297096f4e60d52dcd
Billy <1403809057@qq.com>
2018-08-18 12:02:55 +0800

    finished:add song list dynamically,custom scrollbar,Aug18

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 $(function () {
-//    0.定义全局变量
+//    0.定义全局变量/初始化相关参数
+//    全局变量
     let $audio = $('audio');
     let player = new Player($audio);
+//    初始化滚动条
+    $(".music-box").mCustomScrollbar();
 
 //    1.动态添加歌曲列表
     getPlayerList();
@@ -14,10 +17,11 @@ $(function () {
                 // 1.1遍历获取到的数据, 创建每一条音乐
                 let $musicList = $(".music-list");
                 $.each(data, function (index, ele) {
-                    // let $item = crateMusicItem(index, ele);
-                    // $musicList.append($item);
+                    //创建音乐并插入
+                    let $item = crateMusicItem(index, ele);
+                    $musicList.append($item);
                 });
-                //
+                //1.2初始化页面信息
                 // initMusicInfo(data[0]);
                 // initMusicLyric(data[0]);
             },
@@ -70,5 +74,43 @@ $(function () {
         $(this).parents('.music-list-content').remove();
     //    5.2更改剩余歌曲序号
 
-    })
+    });
+
+    /**
+     * 动态创建一条歌曲表单
+     * @param index
+     * @param ele
+     * @returns {jQuery|HTMLElement}
+     */
+    function crateMusicItem(index,ele) {
+        let $music = $(`<li class="music-list-content">
+                            <div class="checkbox"><span></span></div>
+                            <div class="index"><a>${index + 1}</a></div>
+                            <div class="title">
+                                <div class="title-text"><a title="${ele.name}">${ele.name}</a></div>
+                                <div class="title-control">
+                                    <a class="stop" href="javascript:;" title="暂停"></a>
+                                    <a class="play" href="javascript:;" title="播放"></a>
+                                    <a href="javascript:;" title="添加到歌单"></a>
+                                    <a href="javascript:;" title="下载"></a>
+                                    <a href="javascript:;" title="分享"></a>
+                                </div>
+                            </div>
+                            <div class="singer"><a href="javascript:;" title="${ele.singer}">${ele.singer}</a></div>
+                            <div class="duration">
+                                <a class="duration-time">${ele.time}</a>
+                                <a class="del" href="javascript:;" title="删除"></a>
+                            </div>
+                        </li>`);
+
+        //初始化这个歌曲单元的信息
+        $music.get(0).index = index;
+        $music.get(0).music = ele;
+        return $music;
+    }
 });
+
+
+    function initMusicInfo(ele) {
+
+    }
